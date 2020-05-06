1a70b861e7549dc561b5356a7df0743b9cfd262d
AnnaDanilina <avd09@mail.ru>
2020-05-06 22:44:07 +0300

    HW0207_1

--- src/clientchat/Controller.java
+++ src/clientchat/Controller.java
@@ -3,8 +3,10 @@ package clientchat;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -21,20 +23,81 @@ public class Controller implements Initializable {
     @FXML
     public TextField jtf;
 
+    public TextField loginField;
+    public PasswordField passField;
+
+    public HBox bottomPanel;
+    public HBox upperPanel;
+
+    private boolean authorized;
     private Socket socket;
     private DataInputStream in;
     private DataOutputStream out;
 
-    public void sendMsg() {
-        String str = jtf.getText();
-        if (str.length() > 0) {
-            try {
-                out.writeUTF(jtf.getText());
-                jtf.clear();
-                jtf.requestFocus();
-            } catch (IOException e) {
-                e.printStackTrace();
+    public void setAuthorized(boolean authorized) {
+        this.authorized = authorized;
+        if (!this.authorized) {
+            upperPanel.setVisible(true);
+            bottomPanel.setVisible(false);
+        } else {
+            upperPanel.setVisible(false);
+            bottomPanel.setVisible(true);
+            jta.clear();
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            socket = new Socket("localhost", 8189);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            setAuthorized(false);
+            Thread t = new Thread(() -> {
+                try {
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.startsWith("/authok")) {
+                            setAuthorized(true);
+                            break;
+                        }
+                        jta.appendText(str + "\n");
+                    }
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.equals("/end")) {
+                            break;
+                        }
+                        jta.appendText(str + "\n");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    setAuthorized(false);
+                }
+            });
+            t.setDaemon(true);
+            t.start();
+        } catch (IOException e) {
+            Platform.exit();
+        }
+    }
+
+    public void onSendMsg() {
+        try {
+            out.writeUTF(jtf.getText());
+            if (jtf.getText().equals("/end")) {
+                socket.close();
             }
+            jtf.clear();
+            jtf.requestFocus();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -51,26 +114,13 @@ public class Controller implements Initializable {
         }
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void onAuthClick() {
         try {
-            socket = new Socket("localhost", 87);
-            in = new DataInputStream(socket.getInputStream());
-            out = new DataOutputStream(socket.getOutputStream());
-            new Thread(() -> {
-                try {
-                    while (true) {
-                        String str = in.readUTF();
-                        jta.appendText(str + "\n");
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }).start();
-
-        } catch (IOException e) {
-            Platform.exit();
+            out.writeUTF("/auth " + loginField.getText() + " " + passField.getText());
+            loginField.clear();
+            passField.clear();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
     }
 }
--- src/clientchat/Main.java
+++ src/clientchat/Main.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("clientchat.fxml"));
         primaryStage.setTitle("Client chat");
         primaryStage.setScene(new Scene(root, 600, 400));
